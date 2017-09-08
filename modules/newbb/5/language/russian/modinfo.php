352cdab954af2a0521f1f281dafc95b4cbcf6387
Nikolay N Shmelev <aerograf@shmel.org>
2017-09-08 10:11:34 +0300

    tpl

--- modules/newbb/5/language/russian/modinfo.php
+++ modules/newbb/5/language/russian/modinfo.php
@@ -257,7 +257,7 @@ define('_MI_NEWBB_POLL_MODULE_DESC', 'XoopsPoll или клон. Оставьт�
 
 //Help
 define('_MI_NEWBB_DIRNAME', basename(dirname(dirname(__DIR__))));
-define('_MI_NEWBB_HELP_HEADER', __DIR__.'/help/helpheader.html');
+define('_MI_NEWBB_HELP_HEADER', __DIR__.'/help/helpheader.tpl');
 define('_MI_NEWBB_BACK_2_ADMIN', 'Назад к Администрации ');
 define('_MI_NEWBB_OVERVIEW', 'Обзор');
 
