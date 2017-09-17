ff1db6bb36a1e76555ac1617a79adb53b1b0dfbd
Nikolay N Shmelev <aerograf@shmel.org>
2017-09-17 16:44:19 +0300

    Doublen Help

--- modules/myiframe/language/russian/modinfo.php
+++ modules/myiframe/language/russian/modinfo.php
@@ -14,7 +14,7 @@ define('_MI_MYIFRAME_DESC', 'Вы можете управлять большим
 
 // Pour le menu d'admin
 define('_MI_MYIFRAME_ADMENU1', 'Управление iFrames');
-define('_MI_MYIFRAME_ADMENU2', 'About');
+define('_MI_MYIFRAME_ADMENU2', 'О модуле');
 
 // New in version 1.4 *******************************************************************************************
 define('_MI_MYIFRAME_OPT0', 'Отображать фреймы в меню Xoops?');
@@ -23,4 +23,15 @@ define('_MI_MYIFRAME_OPT0_DSC', 'С помощью этой опции Вы мо
 define('_MI_MYIFRAME_OPT1', 'Отображать список iframes?');
 define('_MI_MYIFRAME_OPT1_DSC', 'Когда страница модуля вызывается без параметров,<br /> на странице отображается список доступных фреймов');
 
-define('_MI_MYIFAME_BNAME1', 'Показать iframe');
+define('_MI_MYIFRAME_BNAME1', 'Показать iframe');
+
+//Help
+define('_MI_MYIFRAME_DIRNAME', basename(dirname(dirname(__DIR__))));
+define('_MI_MYIFRAME_HELP_HEADER', __DIR__ . '/help/helpheader.tpl');
+define('_MI_MYIFRAME_BACK_2_ADMIN', 'Вернуться в администрирование модуля ');
+define('_MI_MYIFRAME_OVERVIEW', 'Обзор');
+
+//help multi-page
+define('_MI_MYIFRAME_DISCLAIMER', 'Disclaimer');
+define('_MI_MYIFRAME_LICENSE', 'Лицензия');
+define('_MI_MYIFRAME_SUPPORT', 'Поддержка');
