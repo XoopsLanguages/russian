58ab639f88807ea098186571ea997f890ca90af6
Nikolay N Shmelev <aerograf@shmel.org>
2017-11-27 19:30:15 +0300

    Add Content module

---
+++ modules/content/language/russian/blocks.php
@@ -0,0 +1,7 @@
+<?php
+define('_MB_CONTENT_LINKHOME', 'Показать главную ссылку?');
+define('_MB_CONTENT_LINKMODULES', 'Показать ссылки модулей?');
+define('_MB_CONTENT_HOMEPAGE', 'Главная страница');
+define('_MB_CONTENT_ADDPAGE', 'Добавить страницу');
+define('_MB_CONTENT_MENUADDITEM', 'Добавить страницу меню');
+define('_MB_CONTENT_PADDING', 'Отступы/Отступ для подменю');
