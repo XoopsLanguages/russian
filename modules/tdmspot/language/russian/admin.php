037be36224a0db4e49366f18014bb4f2dde63ee4
Nikolay N Shmelev <aerograf@shmel.org>
2017-12-14 00:10:31 +0300

    Add TDMSpot

---
+++ modules/tdmspot/language/russian/admin.php
@@ -0,0 +1,153 @@
+<?php
+
+//formulaire
+define('_AM_TDMSPOT_ADD', 'Добавить');
+define('_AM_TDMSPOT_INDATE', 'Дата');
+define('_AM_TDMSPOT_INDATE_DESC', 'эта статья будет отображаться только с этой даты');
+define('_AM_TDMSPOT_INDATE_TIME', 'Дата и время Вашего сервера <b>%s</b>');
+define('_AM_TDMSPOT_UPDATE', 'Обновить');
+define('_AM_TDMSPOT_EDITER', 'Редактировать');
+define('_AM_TDMSPOT_DELETE', 'Удалить');
+define('_AM_TDMSPOT_BASESURE', 'ПРЕДУПРЕЖДЕНИЕ: Вы действительно хотите удалить?');
+define('_AM_TDMSPOT_BASESUREDEL', "ПРЕДУПРЕЖДЕНИЕ: Вы уверены, что хотите удалить: <b><span style='color : Red'> %s </span></b> ?");
+define(
+    '_AM_TDMSPOT_BASESUREDELCAT',
+    "ПРЕДУПРЕЖДЕНИЕ: Вы уверены, что хотите удалить: <b><span style='color : Red'> %s </span></b>, дети категории будут удалены, но файлы не будут удалены"
+);
+define('_AM_TDMSPOT_BASEOK', 'Обновление базы данных');
+define('_AM_TDMSPOT_PAGEERROR', 'Ошибка: Вы должны создать страницу');
+define('_AM_TDMSPOT_CATERROR', 'Ошибка: Вы должны создать категорию');
+
+define('_AM_TDMSPOT_OPTIONDESC', 'по умолчанию и оригинального блока вы можете изменить параметры, отредактировав этот блок после записи');
+
+define(
+    '_AM_TDMSPOT_PAGEDESC',
+    'Manage here your index page, a page can display multiple Blocks and several items defined by their class and limited by the "limit". You can navigate between pages by several effects settings are in the preferences of the module. '
+);
+
+define('_AM_TDMSPOT_MANAGE_INDEX', 'Главная модуля');
+define('_AM_TDMSPOT_MANAGE_PAGE', 'Управление страницами');
+define('_AM_TDMSPOT_MANAGE_ITEM', 'Управление пунктами ');
+define('_AM_TDMSPOT_THEREARE_PAGE', "Есть <span style='color: #ff0000; font-weight: bold'>%s</span> страниц(ы) в базе данных");
+define('_AM_TDMSPOT_MANAGE_BLOCK', 'Управление блоками');
+define('_AM_TDMSPOT_MANAGE_CAT', 'Управление категориями');
+define('_AM_TDMSPOT_THEREARE_BLOCK', "Есть <span style='color: #ff0000; font-weight: bold'>%s</span> блок(ов) в базе данных");
+define('_AM_TDMSPOT_THEREARE_CAT', "Есть <span style='color: #ff0000; font-weight: bold'>%s</span> категория(й) в базе данных");
+define('_AM_TDMSPOT_THEREARE_ITEM', "Есть <span style='color: #ff0000; font-weight: bold'>%s</span> статья(ей) в базе данных");
+define('_AM_TDMSPOT_THEREARE_ITEM_WAITING', "Есть <span style='color: #ff0000; font-weight: bold'>%s</span> статья(ей), ожидающих в базе данных");
+define('_AM_TDMSPOT_THEREARE_ITEM_TIME', "Есть <span style='color: #ff0000; font-weight: bold'>%s</span> статья(ей) в базе данных");
+define('_AM_TDMSPOT_THEREARE_CAT_WAITING', "Есть <span style='color: #ff0000; font-weight: bold'>%s</span> категория(й) ожидающих в базе данных");
+
+define('_AM_TDMSPOT_MANAGE_PLUG', 'Управление плагинами');
+define('_AM_TDMSPOT_MANAGE_PLUGERROR', 'Скопируйте файл "xoops_plugins/function.xoSpot.php" в папку Root: "/class/smarty/xoops_plugins/"');
+define('_AM_TDMSPOT_MANAGE_PLUGOK', 'Установленные плагины ');
+define('_AM_TDMSPOT_MANAGE_UPDATE', 'Обновить');
+define('_AM_TDMSPOT_MANAGE_ABOUT', 'О модуле');
+define('_AM_TDMSPOT_MANAGE_HELP', 'Помощь');
+
+define('_AM_TDMSPOT_TITLE', 'Название');
+define('_AM_TDMSPOT_BLOCK', 'Показать блок');
+define('_AM_TDMSPOT_PAGE', 'Страница');
+define('_AM_TDMSPOT_OPTION', 'Доступные варианты');
+define('_AM_TDMSPOT_NOOPTION', 'Нет параметров');
+
+define('_AM_TDMSPOT_CENTER', 'Показать');
+
+define('_AM_TDMSPOT_CENTERCCOLUMN', 'Вверху по центру');
+define('_AM_TDMSPOT_CENTERLCOLUMN', 'Вверху слева');
+define('_AM_TDMSPOT_CENTERRCOLUMN', 'Вверху справа');
+
+define('_AM_TDMSPOT_BOTTOMCCOLUMN', 'Внизу по центру');
+define('_AM_TDMSPOT_BOTTOMLCOLUMN', 'Внизу слева');
+define('_AM_TDMSPOT_BOTTOMRCOLUMN', 'Внизу справа');
+
+define('_AM_TDMSPOT_MORPHOCENTERCCOLUMN', 'Mor.pho - Вверху по центру');
+define('_AM_TDMSPOT_MORPHOCENTERLCOLUMN', 'Mor.pho - Вверху слева');
+define('_AM_TDMSPOT_MORPHOCENTERRCOLUMN', 'Mor.pho - Вверху справа');
+
+define('_AM_TDMSPOT_MORPHOBOTTOMCCOLUMN', 'Mor.pho - Внизу по центру');
+define('_AM_TDMSPOT_MORPHOBOTTOMLCOLUMN', 'Mor.pho - Внизу слева');
+define('_AM_TDMSPOT_MORPHOBOTTOMRCOLUMN', 'Mor.pho - Внизу справа');
+
+define('_AM_TDMSPOT_ALL', 'Все');
+define('_AM_TDMSPOT_WEIGHT', 'Вес');
+define('_AM_TDMSPOT_SEARCH', 'Поиск');
+define('_AM_TDMSPOT_ID', 'ID');
+define('_AM_TDMSPOT_VISIBLE', 'Видимый');
+define('_AM_TDMSPOT_FREE', 'Свободно');
+define(
+    '_AM_TDMSPOT_FREEDESC',
+    'Летающая страница? Показать меню можно с помощью <{xospot}> или скопировать в админ панеле ссылку и разместить у себя в контексте.'
+);
+define('_AM_TDMSPOT_ACTION', 'Действие');
+define('_AM_TDMSPOT_PARENT', 'Родитель');
+define('_AM_TDMSPOT_CATEGORY', 'Категория');
+define('_AM_TDMSPOT_LIMIT', 'Лимит');
+define('_AM_TDMSPOT_TEXT', 'Текст');
+define('_AM_TDMSPOT_IMG', 'Изображение');
+define('_AM_TDMSPOT_PATH', 'Путь к файлу %s');
+define('_AM_TDMSPOT_UPLOAD', 'Загрузить');
+define('_AM_TDMSPOT_FILE', 'Файл');
+define('_AM_TDMSPOT_TEXT_DESC', 'Полезные теги: <br>
+{X_BREAK}: задайте размер краткого описания.<br>
+{X_NAME}: отобразит имя пользователя.<br>
+{X_UNAME}: отобразит имя пользователя.<br>
+{X_UEMAIL}: отобразит email пользователя.<br>
+{X_ADMINMAIL}: отобразит email администратора.<br>
+{X_SITENAME}: будет отображаться имя Вашего сайта.<br>
+{X_SITEURL}: отобразит URL Вашего сайта.<br><br>
+{X_PAGE: montitre}: показать сводку');
+
+//plug.php
+define('_AM_TDMSPOT_PLUGDEF', 'Страница по умолчанию');
+define('_AM_TDMSPOT_PLUGNONE', 'Нет');
+define('_AM_TDMSPOT_PLUGPAGE', 'Показывать на странице');
+define('_AM_TDMSPOT_PLUGDISPLAY', 'Показать');
+define('_AM_TDMSPOT_PLUGALL', 'ВСЕ');
+define('_AM_TDMSPOT_PLUGTEXT', 'Текст');
+define('_AM_TDMSPOT_PLUGTABS', 'Вкладки');
+define('_AM_TDMSPOT_PLUGSELECT', 'Выбрать');
+define('_AM_TDMSPOT_PLUG_DESC', 'Используйте этот код в своем шаблоне или теме, чтобы отобразить блоки');
+define('_AM_TDMSPOT_PLUGSTYLE', 'Стиль');
+
+//permission
+define('_AM_TDMSPOT_MANAGE_PERM', 'Управление разрешениями');
+define('_AM_TDMSPOT_PERM_2', 'Разрешить просмотр');
+define('_AM_TDMSPOT_PERM_4', 'публиковать');
+define('_AM_TDMSPOT_PERM_8', 'публикация без подтверждения');
+define('_AM_TDMSPOT_PERM_16', 'создать PDF/Print');
+define('_AM_TDMSPOT_PERM_32', 'может проголосовать');
+define('_AM_TDMSPOT_PERM_64', 'Панель соцсетей (Добавь это)');
+define('_AM_TDMSPOT_PERM_128', 'Кнопка RSS');
+define('_AM_TDMSPOT_PERM_256', 'можно скачать');
+
+//About (about.php)
+define('_AM_ABOUT_RELEASEDATE', 'Дата выпуска');
+define('_AM_ABOUT_AUTHOR', 'Автор');
+define('_AM_ABOUT_CREDITS', 'Кредит');
+define('_AM_ABOUT_README', 'Главная информация');
+define('_AM_ABOUT_MANUAL', 'Помощь');
+define('_AM_ABOUT_LICENSE', 'Лицензия');
+define('_AM_ABOUT_MODULE_STATUS', 'Статус');
+define('_AM_ABOUT_WEBSITE', 'Веб-сайт');
+define('_AM_ABOUT_AUTHOR_NAME', 'Имя автора');
+define('_AM_ABOUT_AUTHOR_WORD', 'Авторское слово');
+define('_AM_ABOUT_CHANGELOG', 'Журнал изменений');
+define('_AM_ABOUT_MODULE_INFO', 'Информация о модуле');
+define('_AM_ABOUT_AUTHOR_INFO', 'Информация об авторе');
+define('_AM_ABOUT_DISCLAIMER', 'Отказ от ответственности');
+define('_AM_ABOUT_DISCLAIMER_TEXT', 'Лицензия GPL - отсутствие гарантий');
+
+//1.02
+define('_AM_TDMSPOT_MANAGE_IMPORT', 'Управление импортом');
+define('_AM_TDMSPOT_IMPORT', 'Импорт');
+define('_AM_TDMSPOT_IMPORT_NONE', 'Не установлен');
+define('_AM_TDMSPOT_IMPORT_NEWS', 'Модуль News');
+define('_AM_TDMSPOT_IMPORT_SMARTSECTION', 'Модуль Smartsection');
+define('_AM_TDMSPOT_IMPORT_WFSECTION', 'Модуль Wf-section');
+define('_AM_TDMSPOT_IMPORT_XFSECTION', 'Модуль Xf-section');
+define('_AM_TDMSPOT_IMPORTDESC', 'Предупреждение всегда начинайте импорт до создания категорий или элементов, Вы потеряете больше голосов и приложите файлы к статье.');
+//1.04
+define('_AM_TDMSPOT_IMPORT_INDISPLAY', 'Импорт и показать');
+define('_AM_TDMSPOT_IMPORT_OUTDISPLAY', 'Импорт и скрыть');
+define('_AM_TDMSPOT_BASEERROR', 'Ошибка: Ваши изменения не сохранены');
